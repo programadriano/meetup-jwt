56b06a0c192d387e94417fea9eeb50191580cab8
programadriano <thiago.adriano26@gmail.com>
2019-07-31 20:32:32 -0300

    add

--- angular-jwt/src/app/pages/login/service/login.service.ts
+++ angular-jwt/src/app/pages/login/service/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { shareReplay, map, catchError } from 'rxjs/operators';
+import { shareReplay, map, catchError, retry, retryWhen, delay, take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { HttpUtilService } from 'src/app/shared/services/http-util.service';
 
@@ -16,8 +16,10 @@ export class LoginService {
 
   login(user) {
     return this.http.post(this.API_URL + '/authentication', user)
-    .pipe(map(this.httpUtil.extrairDados))
-    .pipe(
-      catchError(this.httpUtil.processarErros));
+      .pipe(map(this.httpUtil.extrairDados))
+      .pipe(
+        //retryWhen(errors => errors.pipe(delay(1000), take(10))),
+        //retry(3),
+        catchError(this.httpUtil.processarErros));
   }
 }
